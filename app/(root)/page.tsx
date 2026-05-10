505b8ada53804e366fbb8325381f2b6bb75fde04
Talha Zahid <italhazahid@gmail.com>
2026-05-10 18:10:10 +0500

    Added Components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 import Hero from "@/components/Home/Hero";
 import Features from "@/components/Home/Features";
+import HowRaastahWorks from "@/components/Home/HowRaastahWorks";
+import DownloadSection from "@/components/Home/DownloadSection";
+import TrackRideSection from "@/components/Home/TrackRideSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <HowRaastahWorks />
+      <TrackRideSection />
       <Features />
+      <DownloadSection />
     </>
   );
 }
